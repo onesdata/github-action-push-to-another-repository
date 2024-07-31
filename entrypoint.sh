463eb066b0ceb236d58c5936e496a016a8f80136
Adrián Martínez Guantes <adrian.martinez@onestic.com>
2024-07-31 10:50:43 +0200

    Allow force push option

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@ TARGET_BRANCH="${9}"
 COMMIT_MESSAGE="${10}"
 TARGET_DIRECTORY="${11}"
 CREATE_TARGET_BRANCH_IF_NEEDED="${12}"
+FORCE="${13}"
 
 if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
 then
@@ -177,6 +178,15 @@ git add .
 echo "[+] git commit:"
 git commit --message "$COMMIT_MESSAGE"
 
-echo "[+] Pushing git commit"
-# --set-upstream: sets de branch when pushing to a branch that does not exist
-git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
+
+if [ "$FORCE" = "true" ]
+then
+  echo "[+] Force pushing..."
+  FORCE_FLAG="-f"
+else
+  echo "[+] Pushing..."
+  FORCE_FLAG=""
+fi
+
+# --set-upstream: sets the branch when pushing to a branch that does not exist
+git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH" $FORCE_FLAG
